3dd8149b8ba276197df1d2cb0183b5e17eddec42
alvynben <alvynben@gmail.com>
2021-10-04 14:45:28 +0800

    Change Fridget Logo layout to meet coding standards

--- src/main/java/seedu/ui/Ui.java
+++ src/main/java/seedu/ui/Ui.java
@@ -16,17 +16,17 @@ public class Ui {
      * Prints introductory message when Fridget runs.
      */
     public void printIntroduction() {
-        String logo = "                                                                        \n" +
-                "   ad88              88           88                                    \n" +
-                "  d8\"                \"\"           88                             ,d     \n" +
-                "  88                              88                             88     \n" +
-                "MM88MMM  8b,dPPYba,  88   ,adPPYb,88   ,adPPYb,d8   ,adPPYba,  MM88MMM  \n" +
-                "  88     88P'   \"Y8  88  a8\"    `Y88  a8\"    `Y88  a8P_____88    88     \n" +
-                "  88     88          88  8b       88  8b       88  8PP\"\"\"\"\"\"\"    88     \n" +
-                "  88     88          88  \"8a,   ,d88  \"8a,   ,d88  \"8b,   ,aa    88,    \n" +
-                "  88     88          88   `\"8bbdP\"Y8   `\"YbbdP\"Y8   `\"Ybbd8\"'    \"Y888  \n" +
-                "                                       aa,    ,88                       \n" +
-                "                                        \"Y8bbdP\"                        ";
+        String logo = "                                                                        \n"
+                + "   ad88              88           88                                    \n"
+                + "  d8\"                \"\"           88                             ,d     \n"
+                + "  88                              88                             88     \n"
+                + "MM88MMM  8b,dPPYba,  88   ,adPPYb,88   ,adPPYb,d8   ,adPPYba,  MM88MMM  \n"
+                + "  88     88P'   \"Y8  88  a8\"    `Y88  a8\"    `Y88  a8P_____88    88     \n"
+                + "  88     88          88  8b       88  8b       88  8PP\"\"\"\"\"\"\"    88     \n"
+                + "  88     88          88  \"8a,   ,d88  \"8a,   ,d88  \"8b,   ,aa    88,    \n"
+                + "  88     88          88   `\"8bbdP\"Y8   `\"YbbdP\"Y8   `\"Ybbd8\"'    \"Y888  \n"
+                + "                                       aa,    ,88                       \n"
+                + "                                        \"Y8bbdP\"                        ";
         System.out.println(logo);
         System.out.println("Hello!");
         System.out.println("What is your name?");
