d6cdc8bbc80845a00f54f2b11d96e1a7b1333506
zonglun99 <zonglun99@gmail.com>
2021-10-05 20:43:41 +0800

    Previous updates

---
+++ src/main/java/seedu/fridget/Fridget.java
@@ -0,0 +1,42 @@
+package seedu.fridget;
+
+import seedu.commands.Command;
+import seedu.data.exception.FridgetException;
+import seedu.parser.Parser;
+import seedu.storage.IngredientList;
+import seedu.ui.Ui;
+
+public class Fridget {
+    private final Ui ui;
+    private final Parser parser;
+    private final IngredientList ingredientList;
+
+    /**
+     * Constructor for Fridget.
+     */
+    public Fridget() {
+        ui = new Ui();
+        parser = new Parser();
+        ingredientList = new IngredientList();
+    }
+
+    public void run() {
+        ui.printIntroduction();
+
+        try {
+            String userInput = ui.readUserInput();
+            Command c = parser.parseCommand(userInput);
+            c.execute(ui, parser, ingredientList);
+        } catch (FridgetException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Main entry-point for the java.fridget.Fridget application.
+     */
+    public static void main(String[] args) {
+        new Fridget().run();
+    }
+}
