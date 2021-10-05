1985a87fcc14683bee4e896cb95fe47f3bbae4b8
BryanElmer <bryan.elmer.mulijono@gmail.com>
2021-10-05 23:30:10 +0800

    Add instance of storage in Fridget.java

--- src/main/java/seedu/fridget/Fridget.java
+++ src/main/java/seedu/fridget/Fridget.java
@@ -4,20 +4,23 @@ import seedu.commands.Command;
 import seedu.data.exception.FridgetException;
 import seedu.parser.Parser;
 import seedu.storage.IngredientList;
+import seedu.storage.Storage;
 import seedu.ui.Ui;
 
 public class Fridget {
     private final Ui ui;
     private final Parser parser;
     private final IngredientList ingredientList;
+    private final Storage storage;
 
     /**
      * Constructor for Fridget.
      */
-    public Fridget() {
+    public Fridget(String listFilePath, String logFilePath) {
         ui = new Ui();
         parser = new Parser();
         ingredientList = new IngredientList();
+        storage = new Storage(ingredientList, parser, listFilePath, logFilePath);
     }
 
     public void run() {
@@ -37,6 +40,6 @@ public class Fridget {
      * Main entry-point for the java.fridget.Fridget application.
      */
     public static void main(String[] args) {
-        new Fridget().run();
+        new Fridget("config/savedList", "config/savedLog").run();
     }
 }
