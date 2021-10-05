f7224a47d853bd5505a1f033c933725545a8cbd0
BryanElmer <bryan.elmer.mulijono@gmail.com>
2021-10-05 23:30:42 +0800

    Add function to initialise storage file

--- src/main/java/seedu/storage/Storage.java
+++ src/main/java/seedu/storage/Storage.java
@@ -1,13 +1,52 @@
 package seedu.storage;
 
+import seedu.data.ingredient.Ingredient;
+import seedu.parser.Parser;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Storage {
-    private final String filePath;
+    private final IngredientList ingredientList;
+    private final Parser parser;
+
+    private final String fileDirectory;
+    private final String listFilePath;
+    private final String logFilePath;
+
+    private static final String REGEX_DATA_SEPARATOR = " \\| ";
 
     /**
      * A constructor to save data into text file.
-     * @param filePath pathway of file storage.
+     * @param listFilePath pathway of ingredient list file storage.
+     * @param logFilePath pathway of user log file storage.
      */
-    public Storage(String filePath) {
-        this.filePath = filePath;
+    public Storage(IngredientList ingredientList, Parser parser, String listFilePath, String logFilePath) {
+        String[] fileComponents = listFilePath.split("/");
+        this.ingredientList = ingredientList;
+        this.parser = parser;
+        this.listFilePath = listFilePath;
+        this.logFilePath = logFilePath;
+        this.fileDirectory = fileComponents[0];
+
+        try {
+            loadFile();
+        } catch (IOException e) {
+            System.out.println("Error while trying to load existing file.");
+        }
+    }
+
+    private void loadFile() throws IOException {
+        File dataDirectory = new File(fileDirectory);
+        File listFile = new File(listFilePath);
+        File logFile = new File(logFilePath);
+
+        if (!dataDirectory.exists()) {
+            dataDirectory.mkdir();
+            listFile.createNewFile();
+            logFile.createNewFile();
+            return;
+        }
     }
 }
