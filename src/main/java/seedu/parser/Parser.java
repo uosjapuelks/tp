d39b76b418ce9a64e0dde53974fbdbb2a4b9af11
alvynben <alvynben@gmail.com>
2021-10-06 21:18:22 +0800

    Implement "find" command to find ingredients matching search term

--- src/main/java/seedu/parser/Parser.java
+++ src/main/java/seedu/parser/Parser.java
@@ -1,9 +1,6 @@
 package seedu.parser;
 
-import seedu.commands.AddCommand;
-import seedu.commands.ExitCommand;
-import seedu.commands.Command;
-import seedu.commands.HelpCommand;
+import seedu.commands.*;
 import seedu.data.exception.FridgetException;
 import seedu.data.ingredient.Ingredient;
 
@@ -31,6 +28,8 @@ public class Parser {
             return new ExitCommand();
         case "help":
             return new HelpCommand();
+        case "find":
+            return new FindCommand();
         default:
             throw new FridgetException("No command found!");
         }
@@ -46,5 +45,16 @@ public class Parser {
         String ingredientName = splitUserInput[1];
         LocalDate expiryDate = LocalDate.parse(splitUserInput[2].replace("/", ""));
         return new Ingredient(ingredientName, expiryDate);
+    }
+
+    /**
+     * Returns a search term provided by the "find" command.
+     * @param userInput The input from the user in this manner - "find burger".
+     * @return The search term.
+     */
+    public String parseSearchTermFromFinding(String userInput) {
+        String[] splitUserInput = userInput.trim().split(" ", 2);
+        String searchTerm = splitUserInput[1];
+        return searchTerm;
     }
 }
