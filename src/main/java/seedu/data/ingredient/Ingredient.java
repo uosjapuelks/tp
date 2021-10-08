782481cb102134bade3b086e6c783a34eb096560
Anderson <andersonleong@outlook.com>
2021-10-08 21:04:15 +0800

    Added comparator into Ingredient to compare by name or by expiryDate.

--- src/main/java/seedu/data/ingredient/Ingredient.java
+++ src/main/java/seedu/data/ingredient/Ingredient.java
@@ -1,6 +1,7 @@
 package seedu.data.ingredient;
 
 import java.time.LocalDate;
+import java.util.Comparator;
 
 public class Ingredient {
     protected String ingredientName;
@@ -28,4 +29,38 @@ public class Ingredient {
     public String getIngredientName() {
         return ingredientName;
     }
+
+    public LocalDate getExpiryDate() {
+        return expiryDate;
+    }
+
+    /**
+     * Comparator that compares Strings of description of ingredients.
+     */
+    public static Comparator<Ingredient> IngNameComparator = new Comparator<Ingredient>() {
+        @Override
+        public int compare(Ingredient i1, Ingredient i2) {
+
+            String ingredint1 = i1.getIngredientName().toLowerCase();
+            String ingredint2 = i2.getIngredientName().toLowerCase();
+
+            //ascending order
+            return ingredint1.compareTo(ingredint2);
+        }
+    };
+
+    /**
+     * Comparator that compares LocalDates of the ingredients.
+     */
+    public static Comparator<Ingredient> IngExpiryComparator = new Comparator<Ingredient>() {
+        @Override
+        public int compare(Ingredient i1, Ingredient i2) {
+
+            LocalDate ingExpiry1 = i1.getExpiryDate();
+            LocalDate ingExpiry2 = i2.getExpiryDate();
+
+            //Ascending order
+            return ingExpiry1.compareTo(ingExpiry2);
+        }
+    };
 }
