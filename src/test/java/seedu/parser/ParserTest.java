6100b8e11a9a7cb9a1e8fe37874eafa1d24fc734
alvynben <alvynben@gmail.com>
2021-10-09 23:18:32 +0800

    Add JUnit Tests for Parser

---
+++ src/test/java/seedu/parser/ParserTest.java
@@ -0,0 +1,41 @@
+package seedu.parser;
+
+import org.junit.jupiter.api.Test;
+import seedu.commands.AddCommand;
+import seedu.data.exception.FridgetException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParserTest {
+    Parser parser = new Parser();
+
+    @Test
+    void parseCommand_correctAddCommandFormat_success() throws FridgetException {
+        try {
+            String inputString = "add burger /2021-12-12";
+            assertTrue(parser.parseCommand(inputString) instanceof AddCommand);
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    void parseCommand_noCommandWordInInput_exceptionThrown() {
+            assertThrows(FridgetException.class, () -> {
+                parser.parseCommand("complete nonsense");
+            });
+    }
+
+
+//    @Test
+//    void parseIngredientForAdding() {
+//    }
+//
+//    @Test
+//    void parseSortTypeForList() {
+//    }
+//
+//    @Test
+//    void parseSearchTermFromFinding() {
+//    }
+}
