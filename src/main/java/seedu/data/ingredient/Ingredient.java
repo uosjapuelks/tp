4b9a18c7b0135b477d8478f260408c97f987097a
Anderson <andersonleong@outlook.com>
2021-10-10 03:57:55 +0800

    Added isExpiring() method. Modified toString()

--- src/main/java/seedu/data/ingredient/Ingredient.java
+++ src/main/java/seedu/data/ingredient/Ingredient.java
@@ -1,6 +1,8 @@
 package seedu.data.ingredient;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.Comparator;
 
 public class Ingredient {
@@ -9,29 +11,54 @@ public class Ingredient {
 
     /**
      * Constructor for Ingredient.
+     *
      * @param ingredientName name of the ingredient.
-     * @param expiryDate date of expiry for ingredient.
+     * @param expiryDate     date of expiry for ingredient.
      */
     public Ingredient(String ingredientName, LocalDate expiryDate) {
         this.ingredientName = ingredientName;
         this.expiryDate = expiryDate;
     }
 
+    /**
+     * @return Ingredient Name, and red expiry if isExpiring is true and green if isExpiring is false.
+     */
     @Override
     public String toString() {
-        return ingredientName + " | " + expiryDate;
+        String red = "\u001B[31m%s\u001B[0m";
+        String green = "\u001B[32m%s\u001B[0m";
+        String expiry = expiryDate.format((DateTimeFormatter.ofPattern("dd MMM yyyy")));
+        String decideRedGreen = (this.isNearExpiry()) ? red : green;
+        return ingredientName + " | " + String.format(decideRedGreen, expiry);
     }
 
     /**
      * Gets the Ingredient's name.
+     *
      * @return Ingredient's name.
      */
     public String getIngredientName() {
         return ingredientName;
     }
 
+    /**
+     * Gets the Ingredient's Expiry date.
+     *
+     * @return Ingredient's Expiry date.
+     */
     public LocalDate getExpiryDate() {
         return expiryDate;
+    }
+
+    /**
+     * Check if Ingredient nearing expiry by 7 DAYS.
+     *
+     * @return true if nearing expiry and false if not nearing expiry.
+     */
+    public boolean isNearExpiry() {
+        LocalDate today = LocalDate.now();
+        long daysRemaining = today.until(expiryDate, ChronoUnit.DAYS);
+        return (daysRemaining < 7);
     }
 
     /**
