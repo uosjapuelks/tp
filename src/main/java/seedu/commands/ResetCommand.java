731974beaee81e39278d859e8b852cc38ee0b38c
BryanElmer <bryan.elmer.mulijono@gmail.com>
2021-10-26 20:32:18 +0800

    Fix reset bug when fridge is empty

--- src/main/java/seedu/commands/ResetCommand.java
+++ src/main/java/seedu/commands/ResetCommand.java
@@ -1,5 +1,6 @@
 package seedu.commands;
 
+import seedu.data.exception.FridgetException;
 import seedu.parser.Parser;
 import seedu.storage.IngredientList;
 import seedu.storage.ShoppingList;
@@ -19,9 +20,14 @@ public class ResetCommand extends Command {
      * @param parser The parser object to parse user inputs.
      * @param ingredientList The ingredientList object.
      * @param shoppingList The shoppingList object.
+     * @throws FridgetException The error object thrown.
      */
     @Override
-    public void execute(Ui ui, Parser parser, IngredientList ingredientList, ShoppingList shoppingList) {
+    public void execute(Ui ui, Parser parser, IngredientList ingredientList, ShoppingList shoppingList)
+            throws FridgetException {
+        if (ingredientList.getIngredientList("r").isEmpty()) {
+            throw new FridgetException("You currently have nothing in your fridge.");
+        }
         if (ui.getResetReconfirm()) {
             ingredientList.resetList();
             ui.printResetMessage();
