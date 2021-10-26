586d84cc9bf9c2bccec133c34b5efa59e1e27f16
zonglun99 <zonglun99@gmail.com>
2021-10-26 16:49:55 +0800

    Update help command with new feature.

--- src/main/java/seedu/commands/HelpCommand.java
+++ src/main/java/seedu/commands/HelpCommand.java
@@ -2,18 +2,17 @@ package seedu.commands;
 
 import seedu.parser.Parser;
 import seedu.storage.IngredientList;
-import seedu.notification.Notification;
 import seedu.ui.Ui;
 
 public class HelpCommand extends Command {
     private static final String COMMAND_LIST = "Generating command list...\n"
             + "__________________________________________\n"
             + "List of commands available:\n\n"
-            + "add <INGREDIENT_NAME> </EXPIRY_DATE(format: yyyy-mm-dd)> [eg. add bacon /2022-11-11]\n"
+            + "add INGREDIENT_NAME /EXPIRY_DATE(format: yyyy-mm-dd) [eg. add bacon /2022-11-11]\n"
             + "-> Adds an ingredient and its expiry date to the ingredient list.\n\n"
-            + "find <KEYWORD> [eg. find bacon]\n"
+            + "find KEYWORD [eg. find bacon]\n"
             + "-> Find and print all ingredient associated with keyword.\n\n"
-            + "remove <INGREDIENT_NAME> [eg. delete bacon]\n"
+            + "remove INGREDIENT_NAME [eg. delete bacon]\n"
             + "-> Removes the ingredient from the ingredient list.\n\n"
             + "list -e\n"
             + "-> List out all the ingredients starting with earliest expiry date.\n\n"
@@ -22,7 +21,9 @@ public class HelpCommand extends Command {
             + "list\n"
             + "-> List out all the ingredients in alphabetical order.\n\n"
             + "expiring\n"
-            + "-> Prints out ingredients that have expired or are expiring within a week\n\n"
+            + "-> Prints out ingredients that have expired or are expiring within a week.\n\n"
+            + "update INGREDIENT_NAME [eg. update egg]\n"
+            + "-> Prompts a quantity change for the specified item name.\n\n"
             + "notifs\n"
             + "-> Toggle the notification on or off depending on previous state. Default mode is on.\n\n"
             + "reset\n"
