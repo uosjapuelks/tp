2f20238d98e0abde74d1179d92e3dca4328ee139
zonglun99 <zonglun99@gmail.com>
2021-11-06 15:32:54 +0800

    Added more JUnit testing to UI

--- src/test/java/seedu/ui/UiTest.java
+++ src/test/java/seedu/ui/UiTest.java
@@ -1,9 +1,13 @@
 package seedu.ui;
 
 import org.junit.jupiter.api.Test;
+import seedu.data.exception.FridgetException;
+import seedu.data.item.Item;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class UiTest {
     Ui ui = new Ui();
@@ -24,5 +28,27 @@ class UiTest {
     void sortTypeMessage_eSortTypeInput_expectByExpiryMessage() {
         String sortTypeString = "e";
         assertEquals("List sorted by expiry date:", ui.sortTypeMessage(sortTypeString));
+    }
+
+    //@@ zonglun99
+    @Test
+    void matchItem_itemNotInMatchingListInput_exceptionThrown() {
+        ArrayList<Item> matchingItems = new ArrayList<>();
+        String targetItem = "chicken";
+        assertThrows(FridgetException.class, () -> {
+            ui.matchItem(matchingItems, targetItem, Ui.CommandType.UPDATE);
+        });
+    }
+
+    @Test
+    void getResetQuestion_resetCommandType_expectResetConfirmationMessage() {
+        assertEquals("Are you sure you want to reset everything in the fridge? (Y/N)",
+                ui.getResetQuestion(Ui.CommandType.RESET));
+    }
+
+    @Test
+    void getShopResetQuestion_shopResetCommandType_expectShopResetConfirmationMessage() {
+        assertEquals("Are you sure you want to reset everything in the shopping list? (Y/N)",
+                ui.getResetQuestion(Ui.CommandType.SHOPRESET));
     }
 }
